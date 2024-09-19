376ea1ae16f9c19a364480b41388762b0172fb03
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2024-09-19 13:26:19 -0300

    feat: projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 let modo = 'light'
 function switchTheme() {
     if (modo == 'light') {
-        document.getElementById('switcher').innerHTML = 'Modo light'
+        document.getElementById('switcher').innerHTML = 'Modo dark'
         document.querySelector('html').className = 'alternative-mode'
         modo = 'dark'
     } else {
-        document.getElementById('switcher').innerHTML = 'Modo dark'
+        document.getElementById('switcher').innerHTML = 'Modo light'
         document.querySelector('html').className = 'default-mode'
         modo = 'light'
     }
