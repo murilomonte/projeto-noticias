d5d4bd13f0511dee3957ad9cd7d3ce560f77c126
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2024-09-19 09:32:53 -0300

    feat: home finalizada

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let modo = 'light'
+function switchTheme() {
+    if (modo == 'light') {
+        document.getElementById('switcher').innerHTML = 'Modo light'
+        document.querySelector('html').className = 'alternative-mode'
+        modo = 'dark'
+    } else {
+        document.getElementById('switcher').innerHTML = 'Modo dark'
+        document.querySelector('html').className = 'default-mode'
+        modo = 'light'
+    }
+}
